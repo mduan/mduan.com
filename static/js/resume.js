b66734c511296274965e9277735e8f7da7abb35a
Mack Duan <duan.mack@gmail.com>
2013-12-09 03:45:14 -0800

    Start working on resume

---
+++ static/js/resume.js
@@ -0,0 +1,33 @@
+$(function() {
+
+$('a').each(function() {
+  var $el = $(this);
+  if ($el.hasClass('external')) {
+    $el.attr('target', '_blank');
+    $el.append($('<sup><i class="fa fa-external-link"></i></sup>'));
+  }
+});
+
+$('.fancybox').fancybox({
+  helpers: {
+    title : {
+      type : 'float'
+    }
+  },
+  beforeShow: function() {
+    var alt = this.element.find('.thumbnail').attr('alt');
+    this.inner.find('img').attr('alt', alt);
+    this.title = alt;
+  }
+});
+
+$(window).scroll(function() {
+  var offset = $('.contact').offset().top + $('.contact').outerHeight(true);
+  if (offset - $(this).scrollTop() > 0) {
+    $('.hresume > .lhs').css({ position: 'relative' });
+  } else {
+    $('.hresume > .lhs').css({ position: 'fixed', top: 0 });
+  }
+});
+
+});
