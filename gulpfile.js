dd8df387c620a5ef7b2d29cc8cf870dd924abf13
Mack Duan <duan.mack@gmail.com>
2015-10-30 11:18:00 -0700

    Add jekyll dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp');
+var spawn = require('child_process').spawn;
 
-gulp.task('install', function() {
+gulp.task('install', function(cb) {
+  var spawn = require('child_process').spawn;
+  var install = spawn('bundle', ['install'], {stdio: 'inherit'});
+  install.on('exit', function(code) {
+    cb(code === 0 ? null : 'ERROR: Installation exited with code: ' + code);
+  });
 });
 
-gulp.task('default', function() {
+gulp.task('jekyll', function (cb){
+  // After build: cleanup HTML
+  var jekyll = spawn('bundle', ['install'], {stdio: 'inherit'});
+
+  jekyll.on('exit', function(code) {
+    cb(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+  });
+});
+
+gulp.task('default', ['jekyll'], function() {
   // place code for your default task here
 });
